c8f68e434a3436be97387b7dd56a2b6386dfd6d7
Paulo Sergio <pscosta2008@gmail.com>
2023-10-22 13:57:25 -0300

    feat: Pág Sobre Adicionada; lógica dos cards adicionada

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// abre e fecha cards - sistema 
+const cardsArray = document.getElementsByClassName("cards-container")
+let cardsOpen = [false, false, false, false, false, false];
+
+for(let i = 0; i < cardsArray.length; i++){
+    cardsArray[i].addEventListener("click", function AbreFechaCards(){
+        const descCardsArray = document.getElementsByClassName("desc-cards-container")
+        const arrowCardsArray = document.getElementsByClassName("arrow-cards")
+
+
+        if(cardsOpen[i]){
+            cardsOpen[i] = false;
+            closeAllCards(descCardsArray, arrowCardsArray)
+
+        }
+        else if(!cardsOpen[i]){ // abre
+            cardsOpen = [false, false, false, false, false, false];
+            cardsOpen[i] = true;
+
+            closeAllCards(descCardsArray, arrowCardsArray)
+            
+            descCardsArray[i].style.display = 'block'
+            descCardsArray[i].style.height = '100%'
+            arrowCardsArray[i].style.rotate = '90deg'
+        }
+
+        console.log(cardsOpen)
+        
+        
+    })
+}
+
+function closeAllCards (cards, arrow, ){
+    for(let i = 0; i < cards.length; i++){
+        
+        cards[i].style.display = 'none';
+        arrow[i].style.rotate = '0deg'
+    }
+}
